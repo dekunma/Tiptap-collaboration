2b4c814f2bc7348eb4765699761e41299d7f17d8
dekunma <William@dekun.me>
2021-01-25 21:29:20 +0800

    update config

--- server/app.js
+++ server/app.js
@@ -117,4 +117,4 @@ io.on('connection', async (socket) => {
 
 // start server
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
